1d19847330b5f448978614c4b399048aac94bfd9
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-29 18:22:59 +0200

    Un poco di css per far vedere le thumbnails

--- js/script.js
+++ js/script.js
@@ -10,26 +10,31 @@ createApp({
                         title: 'Marvel\'s Spiderman Miles Morale',
                         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                         isActive: true,
+                        class: "isActive",
                     }, {
                         image: 'img/02.webp',
                         title: 'Ratchet & Clank: Rift Apart',
                         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
                         isActive: false,
+                        class: "noActive",
                     }, {
                         image: 'img/03.webp',
                         title: 'Fortnite',
                         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                         isActive: false,
+                        class: "noActive",
                     }, {
                         image: 'img/04.webp',
                         title: 'Stray',
                         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                         isActive: false,
+                        class: "noActive",
                     }, {
                         image: 'img/05.webp',
                         title: "Marvel's Avengers",
                         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                         isActive: false,
+                        class: "noActive",
                     }
                 ]
         }
@@ -38,10 +43,13 @@ createApp({
         nextSlide() {
             const currentIndex = this.slides.findIndex(slide => slide.isActive);
             this.slides[currentIndex].isActive = false;
+            this.slides[currentIndex].class = "noActive"
             if(currentIndex < this.slides.length - 1){
                 this.slides[currentIndex + 1].isActive = true;
+                this.slides[currentIndex + 1].class = "isActive";
             }else{
                 this.slides[0].isActive = true;
+                this.slides[0].class = "isActive";
             }
             
             
@@ -49,11 +57,22 @@ createApp({
         prevSlide() {
             const currentIndex = this.slides.findIndex(slide => slide.isActive);
             this.slides[currentIndex].isActive = false;
+            this.slides[currentIndex].class = "noActive"
             if(currentIndex > 0){
                 this.slides[currentIndex - 1 ].isActive = true;
+                this.slides[currentIndex - 1].class = "isActive";
             }else{
                 this.slides[this.slides.length - 1].isActive = true;
+                this.slides[this.slides.length - 1].class = "isActive";
             }           
-        }
+        },
+        aggiungiClasse:function( elemento ){
+            const currentIndex = this.slides.findIndex(slide => slide.isActive);
+            if(this.slides[currentIndex].class === "isActive"){
+                this.slide.classList.add("isActive");
+            }else{
+                this.slide.classList.remove("isActive");
+            }
+            }
     }
 }).mount('#app')
